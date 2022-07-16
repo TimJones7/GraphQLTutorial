4ef156285d9799e68a3da704e75224cd79e77ca4
Tim Jones <timothy.jones@gmx.us>
2022-07-16 14:37:02 -0400

    Setting up API and queries and such

--- Program.cs
+++ Program.cs
@@ -1,12 +1,21 @@
+using GraphQLTutorial.Schema;
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddGraphQLServer();
+
+//  Add GraphQL with query types
+builder.Services.AddGraphQLServer()
+    .AddQueryType<Query>();
 
 
 
 var app = builder.Build();
 
+
+
 //  This exposes a single GraphQL query as an endpoint
+//  You only need to expose one endpoint for all client queries
+app.UseRouting();
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapGraphQL();
---
+++ Schema/CourseType.cs
@@ -0,0 +1,29 @@
+﻿namespace GraphQLTutorial.Schema
+{
+    public enum Subject
+    {
+        Mathematics,
+        Science,
+        History
+    }
+
+    public class CourseType
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Subject Subject { get; set; }
+
+        [GraphQLNonNullType]
+        public InstructorType Instructor { get; set; }
+
+        public IEnumerable<StudentType> Students { get; set; }
+
+
+        public string Description()
+        {
+            return $"{Name}this is a course";
+        }
+    }
+}
